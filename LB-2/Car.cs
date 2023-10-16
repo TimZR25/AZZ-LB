dcb921d97154e60f98262bbcbdefcc954d080d24
Alex <7zaytsev00@mail.ru>
2023-10-16 22:14:02 +0700

    Часть второй лабораторной работы

---
+++ LB-2/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB3_2
+{
+    internal class Car : Transport
+    {
+        private Engine CarEngine { get; set; }
+
+        public Car(String model, double maxSpeed, int creationYear, double price, int power)
+        {
+            Model = model;
+            MaxSpeed = maxSpeed;
+            CreationYear = creationYear;
+            Price = price;
+            CarEngine = new Engine(power);
+        }
+
+        public override void StartTransport()
+        {
+            CarEngine.TurnOn();
+            Console.WriteLine("Автомобиль отправился в путь");
+        }
+
+        public override void StopTransport()
+        {
+            CarEngine.TurnOff();
+            Console.WriteLine("Автомобиль остановился");
+        }
+
+        public override String ToString()
+        {
+            return new string($"Данные машины:\nМодель: {Model}, Максимальная скорость: {MaxSpeed}, " +
+                $"Год создания: {CreationYear}\nСтоимость: {Price}, {CarEngine.ToString()}");
+        }
+    }
+}
---
+++ LB-2/Train.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB3_2
+{
+    internal class Train : Transport
+    {
+        private Engine TrainEngine { get; set; }
+
+        private double CarriageCapacity { get; set; }
+
+        private List<Carriage> Carriages { get; set; }
+
+        public Train(String model, double maxSpeed, int creationYear, double price, int power, List<Carriage> carriages)
+        {
+            Model = model;
+            MaxSpeed = maxSpeed;
+            CreationYear = creationYear;
+            Price = price;
+            TrainEngine = new Engine(power);
+            Carriages = carriages;
+            this.CountCarriageCapacity();
+        }
+
+        public void AddCarriage(Carriage carriage)
+        {
+            Carriages.Add(carriage);
+            this.CountCarriageCapacity();
+        }
+
+        private void CountCarriageCapacity()
+        {
+            CarriageCapacity = 0;
+            foreach (Carriage c in Carriages) CarriageCapacity += c.Capacity;
+        }
+
+        public override void StartTransport()
+        {
+            TrainEngine.TurnOn();
+            Console.WriteLine("Поезд отправился в путь");
+        }
+
+        public override void StopTransport()
+        {
+            TrainEngine.TurnOff();
+            Console.WriteLine("Поезд остановился");
+        }
+
+        public override String ToString()
+        {
+            String s = "\nВместимость вагонов поезда:";
+            if (Carriages.Count == 0) { s = ""; }
+
+            for (int i = 0; i < Carriages.Count; i++)
+            {
+                s = s + "\nВагон " + i + " " + Carriages[i].ToString();
+            }
+            return new string($"Данные поезда:\nМодель: {Model}, Максимальная скорость: {MaxSpeed}, " +
+                $"Год создания: {CreationYear}\nСтоимость: {Price}, {TrainEngine.ToString()}, Общая вместимость вагонов: {CarriageCapacity}" + s);
+        }
+    }
+}
